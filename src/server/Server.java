e9cb639720f3135d7342789c6ad0ed51903564c8
Paulo Modesto <paulo.jmodesto@hotmail.com>
2020-04-29 23:55:51 -0300

    Atualizacao no codigo e criacao dos metodos efetuarLogin e listDeUsuarios

--- src/server/Server.java
+++ src/server/Server.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.rmi.server.ExportException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -18,7 +19,7 @@ public class Server extends Thread {
     private String nickName;
     private BufferedReader leitor;
     private PrintWriter escritor;
-    private static final Map<String,Server> clientes = new HashMap<String, Server>();
+    private static final Map<String, Server> clientes = new HashMap<String, Server>();
 
     public Server(Socket cliente) {
         this.cliente = cliente;
@@ -63,40 +64,34 @@ public class Server extends Thread {
         try {
             //Scanner entrada = new Scanner(cliente.getInputStream());
             leitor = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-            escritor = new PrintWriter(cliente.getOutputStream(),true);
+            escritor = new PrintWriter(cliente.getOutputStream(), true);
             escritor.println("Digite seu nome");
             String msg = leitor.readLine();
-            this.nickName = msg.toLowerCase().replaceAll(",","");
-            escritor.println("ola " + this.nickName);
-            clientes.put(this.nickName, this);
+            efeturaLogin(msg);
 
             while (true) {
                 msg = leitor.readLine();
-                if (msg.equalsIgnoreCase("Exit")){
+                if (msg.equalsIgnoreCase("Exit")) {
                     this.cliente.close();
-                }else if (msg.toLowerCase().startsWith("::msg")){
-                    String nomeDestinatario =  msg.substring(5,msg.length());
+                } else if (msg.toLowerCase().startsWith("::msg")) {
+                    String nomeDestinatario = msg.substring(5, msg.length());
                     System.out.println("Enviado para " + nomeDestinatario);
                     Server destinatario = clientes.get(nomeDestinatario);
-                    if (destinatario == null){
+                    if (destinatario == null) {
                         escritor.println("Cliente nao existe");
-                    }else {
+                    } else {
                         escritor.println("digite uma mensagem para " + destinatario.getNickName());
                         destinatario.getEscritor().println(this.nickName + " disse " + leitor.readLine());
+                        System.out.println(this.nickName + " enviou para " + destinatario.getNickName());
                     }
                     // Listar os nomes
-                }else if (msg.equalsIgnoreCase("::listar")) {
-                    StringBuffer str = new StringBuffer();
-                    for (String c : clientes.keySet()){
-                        str.append(c);
-                        str.append(",");
-                    }
-                    str.delete(str.length()-1,str.length());
-                    escritor.println(str.toString());
+                } else if (msg.equalsIgnoreCase("::listar")) {
+                    listaDeUsuarios();
                 } else {
                     for (String c : clientes.keySet()) {
                         Server destinatario = clientes.get(c);
-                        destinatario.getEscritor().println(this.nickName+ " disse " + msg);
+                        destinatario.getEscritor().println(this.nickName + " disse " + msg);
+                        System.out.println(this.nickName + " enviou pra todos");
                         //escritor.println(c + " Voce disse: " + msg);
                     }
                 }
@@ -105,9 +100,40 @@ public class Server extends Thread {
             //System.out.println("Cliente conectado do IP " + cliente.getInetAddress().getHostAddress() + " finalizou conexão");
             //leitor.close();
             //this.cliente.close();
-        } catch (
-                IOException ex) {
+        } catch (IOException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception e) {
+            System.out.println(this.nickName + " desconectou");
         }
     }
+
+    private void listaDeUsuarios() {
+        StringBuffer str = new StringBuffer();
+        for (String c : clientes.keySet()) {
+            str.append(c);
+            str.append(",");
+        }
+        if (str.length() > 0)
+            str.delete(str.length() - 1, str.length());
+        escritor.println(" usuarios: " + str.toString());
+        System.out.println(" usuarios: " + str.toString());
+    }
+
+    private void efeturaLogin(String msg) throws IOException {
+        while (true) {
+            this.nickName = msg.toLowerCase().replaceAll(",", "");
+            if (this.nickName.isEmpty()) {
+                escritor.println("Nome Fazio digite novamente!");
+            } else if (clientes.containsKey(this.nickName)) {
+                escritor.println("Nome repetido digite novamente!");
+                System.exit(1);
+            } else {
+                escritor.println("ola " + this.nickName);
+                clientes.put(this.nickName, this);
+                System.out.println(nickName + " entrou no chat");
+                break;
+            }
+        }
+    }
+
 }
